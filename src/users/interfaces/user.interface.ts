1f79f27434a8b4d57f3f14498da38eec7c4e3a4f
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-12-07 16:51:37 +0000

    added user interface

---
+++ src/users/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  interests: string[];
+  grewUpIn: string;
+  currentlyLiveIn: string;
+  favouriteAnimal: string;
+}
