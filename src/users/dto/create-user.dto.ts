663261374b3d4dfd1345c924f338bc0100068195
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-12-07 11:25:54 +0000

    added users dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+export class createUserDto {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly email: string;
+  readonly interests: string[];
+  readonly grewUpIn: string;
+  readonly currentlyLiveIn: string;
+  readonly favouriteAnimale: string;
+}
