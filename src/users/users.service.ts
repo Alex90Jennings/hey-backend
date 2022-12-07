9231582e63d419f4ff79b2a1e920153e7c8faa3d
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-12-07 16:56:18 +0000

    added hard coded users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: '1010010110923',
+      firstName: 'Rina',
+      lastName: 'Dog',
+      email: 'rina@dog.com',
+      interests: ['digging', 'eating'],
+      grewUpIn: 'Italy',
+      currentlyLiveIn: 'UK',
+      favouriteAnimal: 'squirrel',
+    },
+    {
+      id: '1010010110925',
+      firstName: 'Alex',
+      lastName: 'Jennings',
+      email: 'alex@dog.com',
+      interests: ['coding', 'eating'],
+      grewUpIn: 'UK',
+      currentlyLiveIn: 'UK',
+      favouriteAnimal: 'wolf',
+    },
+  ];
+}
